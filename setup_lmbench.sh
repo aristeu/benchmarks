6e1b10285a1cc7bbfc592b97258f113d1bf32275
Aristeu Rozanski <aris@ruivo.org>
2012-08-13 13:59:40 -0400

    adding lmbench setup script
    Signed-off-by: Aristeu Rozanski <aris@ruivo.org>

---
+++ setup_lmbench.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+function die
+{
+	echo "Error: $1";
+	exit 1;
+}
+
+mkdir -p /opt && cd opt || die "creating /opt";
+
+(wget -o /dev/null -O - http://www.bitmover.com/lmbench/lmbench3.tar.gz | tar xvz -) || die "downloading lmbench";
+cd lmbench3
+(wget -o /dev/null -O - http://jake.ruivo.org/~aris/lmbench-buildfix.patch | patch -p1) || die "applying build fix";
+make >/dev/null || die "building lmbench";
+
