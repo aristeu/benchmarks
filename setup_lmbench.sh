9d71e62091558c78a06da35b901a650836cd2c0f
Aristeu Rozanski <aris@ruivo.org>
2012-08-13 14:48:36 -0400

    testing before committing is an interesting concept, I might use that someday
    Signed-off-by: Aristeu Rozanski <aris@ruivo.org>

--- setup_lmbench.sh
+++ setup_lmbench.sh
@@ -7,7 +7,7 @@ function die
 
 mkdir -p /opt && cd /opt || die "creating /opt";
 
-(wget -o /dev/null -O - http://www.bitmover.com/lmbench/lmbench3.tar.gz | tar xvz -) || die "downloading lmbench";
+(wget -o /dev/null -O - http://www.bitmover.com/lmbench/lmbench3.tar.gz | tar xvz ) || die "downloading lmbench";
 cd lmbench3
 (wget -o /dev/null -O - http://jake.ruivo.org/~aris/lmbench-buildfix.patch | patch -p1) || die "applying build fix";
 make >/dev/null || die "building lmbench";
