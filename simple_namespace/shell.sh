18b68ffa491a4773c44463363a1dda5ba2800530
Aristeu Rozanski <aris@ruivo.org>
2012-08-15 15:08:42 -0400

    no networking support to prevent unshare flag
    Signed-off-by: Aristeu Rozanski <aris@ruivo.org>

--- simple_namespace/shell.sh
+++ simple_namespace/shell.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+iface="$3";
+
 if [ -z "$1" -o -z "$2" -o -z "$3" -o -z "$4" -o -z "$5" ]; then
 	echo "$0 hostname cgroup_path iface ip router" >&2;
 	exit 1;
 fi
 
-unshare -m -u -i -n /stage2.sh "$1" "$2" "$3" "$4" "$5" "$6";
+net="-n";
+if [ "$iface" = "none" ]; then
+	net="";
+fi
+
+unshare -m -u -i $net /stage2.sh "$1" "$2" "$3" "$4" "$5" "$6";
