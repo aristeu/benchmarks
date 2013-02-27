62670c69033527cb4dcec95a09ce8789b949c0f1
Aristeu Rozanski <arozansk@redhat.com>
2013-02-27 17:18:51 -0500

    more work on stress_namespaces.c
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- stress_tests/stress_namespaces.c
+++ stress_tests/stress_namespaces.c
@@ -3,6 +3,8 @@
 #include <errno.h>
 #include <string.h>
 #include <pthread.h>
+#include <fcntl.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 
 #define NS_NUM 6
@@ -14,6 +16,16 @@ enum nsid {
 	USERNS,
 	UTSNS,
 };
+
+#ifndef __NR_setns
+# ifdef __i386__
+#define __NR_setns		346
+# else
+#define __NR_setns		308
+# endif
+#define setns(a, b) syscall(__NR_setns, a, b)
+#endif
+
 
 const char *nsnames[] = {
 	[IPC] = "ipc",
@@ -45,7 +57,7 @@ int init_thread(struct data *data, int pid)
 		}
 		data->nsfd[i] = fd;
 		if (fstat(fd, &sbuff)) {
-			fprintf(stderr, "Unable to stat %s (%s)\n", buffer,
+			fprintf(stderr, "Unable to stat %s (%s)\n", buff,
 				strerror(errno));
 			goto error;
 		}
@@ -56,7 +68,7 @@ int init_thread(struct data *data, int pid)
 error:
 	i--;
 	while (i-- >= 0) {
-		close(nsfd[i]);
+		close(data->nsfd[i]);
 		data->nsfd[i] = -1;
 	}
 	return 1;
@@ -78,12 +90,12 @@ int change_ns(struct data *data, enum nsid id, int fd)
 		perror ("change_ns: unable to stat new fd");
 		return 1;
 	}
-	close(data->nsfd[id]);
 	if (setns(fd, 0)) {
 		fprintf(stderr, "change_ns: error changing namespace %s (%s)\n",
 			nsnames[id], strerror(errno));
 		return 1;
 	}
+	close(data->nsfd[id]);
 	data->nsfd[id] = fd;
 	data->inum[id] = sbuff.st_ino;
 
@@ -93,34 +105,145 @@ int change_ns(struct data *data, enum nsid id, int fd)
 
 int get_next_different_process(struct data *data, struct dirent **dent)
 {
-	int i, pid;
-	struct dirent *retval;
+	struct dirent *retval, cur;
 	struct data d;
+	DIR *dirp;
+	int i, pid = -1;
 
+	dirp = opendir("/proc");
+	if (dirp == NULL)
+		return -1;
 	while (1) {
-		if (readdir_r(dirp, *dent, &retval)) {
+		if (readdir_r(dirp, &cur, &retval)) {
 			perror("Error reading directory entries");
+			closedir(dirp);
 			return -1;
 		}
-		if (retval == NULL)
+		if (retval == NULL) {
+			closedir(dirp);
 			return -1;
+		}
 		pid = atoi(retval->d_name);
 		if (pid == 0)
 			continue;
-		if (init_thread(&data, pid))
+		if (init_thread(&d, pid)) {
+			closedir(dirp);
 			return -1;
+		}
 		for (i = 0; i < NS_NUM; i++) {
-			if (d->inum[i] != data->inum[i])
-				break;
+			if (d.inum[i] != data->inum[i]) {
+				*dent = retval;
+				cleanup_thread(&d);
+				return pid;
+			}
+		}
+		cleanup_thread(&d);
 	}
-	*dent = retval;
-	cleanup_thread(&d);
+	closedir(dirp);
 
-	return pid;
+	return -1;
 }
 
+/* */
+struct stresser_config {
+	/** maximum number of threads to run, stresser_unit.number will be
+	    ignored if set */
+	unsigned int threads;
+	/** stress type determines the way threads are created and started */
+	enum {
+		STRESS_HORDE,	/* threads start all at once */
+		STRESS_FIFO,	/* threads start as they're created */
+	} stress_type;
+
+	/** pick the unit based on chance of n of 10 passed in
+	    stresser_unit.chance, ignores stresser_unit.number */
+	bool use_chance;
+};
+
+struct stresser_unit {
+	union {
+		unsigned int number;	/* number of threads */
+		unsigned char chance;	/* unit chance in 10 of running */
+	};
+	/** if stresser_config.threads is set and stresser_unit.chance is not
+	    set, determines the proportion 1-100 of this unit's threads. this
+	    allows creating tests that 1/4 of the threads are x, 2/4 are y and
+	    the another 1/4 are z, no matter the total number of threads */
+	unsigned char proportion;
+	
+};
+struct stresser_priv {
+	void *fnpriv;		/* function private data */
+	int fnrc;		/* function retval */
+	bool wait;		/* conditional wait? */
+	pthread_cond_t *cond;	/* conditional wait */
+	struct stresser_unit *u /* stresser unit */
+};
+
+static int _stresser_unit(void *p)
+{
+	struct stresser_priv *priv = p;
+	int rc;
+
+	if (p->wait) {
+		rc = pthread_cond_wait(p->cond, p->mutex);
+		if (rc)
+			return rc;
+	}
+
+	p->fnrc = p->u->fn(p->fnpriv);
+
+	return 0;
+}
+
+static int stresser(struct stress_config *cfg, struct stresser_unit *set,
+		    int n_unit, void *fnpriv)
+{
+	struct stresser_priv *priv;
+	pthread_cont_t cond = PTHREAD_COND_INITIALIZER;
+	int threads, i;
+
+	/* first determine the total number of threads */
+	if (cfg->threads)
+		threads = cfg->threads;
+	else {
+		threads = 0;
+		for (i = 0; i < n_unit; i++)
+			threads += set[i].number;
+		if (threads == 0)
+			return -EINVAL;
+	}
+
+	/* allocating private structures */
+	priv = calloc(sizeof(*priv), threads);
+	if (priv == NULL)
+		return -ENOMEM;
+
+
+}
+/* */
+
+static int _setns_stress_thread(void *data)
+{
+
+}
+
+static int run_setns_stress(struct config *config)
+{
+	stressfn set[] = {{__setns_stress_thread, 10}, {NULL,}};
+
+	return stresser(config, __setns_stress_thread, NULL);
+}
+
+const char *options = "sh";
 int main(int argc, char *argv[])
 {
+	struct stress_config cfg;
+
+	cfg.threads = 1000;
+	cfg.stress_type = STRESS_HORDE;
+
+	run_setns_stress(&cfg);
 	return 0;
 }
 
