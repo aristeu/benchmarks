203eb4ded7b24ff2505ab55735ae04a407eb4ee6
Aristeu Rozanski <arozansk@redhat.com>
2013-03-04 11:39:36 -0500

    stress_namespaces: adding 'stresser' generic thread creation
    will be used when creating many threads is required
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- stress_tests/stress_namespaces.c
+++ stress_tests/stress_namespaces.c
@@ -2,10 +2,14 @@
 #include <dirent.h>
 #include <errno.h>
 #include <string.h>
+#define __USE_GNU 1
 #include <pthread.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/syscall.h>
 
 #define NS_NUM 6
 enum nsid {
@@ -151,146 +155,232 @@ struct stresser_config {
 	unsigned int threads;
 	/** stress type determines the way threads are created and started */
 	enum {
-		STRESS_HORDE,	/* threads start all at once */
-		STRESS_FIFO,	/* threads start as they're created */
+		STRESST_HORDE,	/* threads start all at once */
+		STRESST_FIFO,	/* threads start as they're created */
 	} stress_type;
 
-	/** pick the unit based on chance of n of 10 passed in
-	    stresser_unit.chance, ignores stresser_unit.number.
-	    Must have 2 or more units to use chance */
-	bool use_chance;
+	enum {
+		/* threads are created randomly, based on the units' chance */
+		STRESSD_RANDOM,
+		/* each unit has a fixed number of threads created, 'threads'
+		 * is ignored */
+		STRESSD_FIXED,
+		/* proportional: 1-100 proportion for each unit. make sure the
+		 * sum of all units is 100 */
+		STRESSD_PROP,
+	} distribution;
 };
 
 struct stresser_unit {
 	union {
-		unsigned int number;	/* number of threads */
-		unsigned char chance;	/* unit chance in 10 of running */
-	};
-	/** if stresser_config.threads is set and stresser_unit.chance are not
-	    set, determines the proportion 1-100 of this unit's threads. this
-	    allows creating tests that 1/4 of the threads are x, 2/4 are y and
-	    the another 1/4 are z, no matter the total number of threads */
-	unsigned char proportion;
+		unsigned int number;		/* fixed number of threads */
+		unsigned char chance;		/* unit chance 1-10 of running */
+		unsigned char proportion;	/* 1-100 proportion this unit will run */
+	} d;
 	int (*fn)(void *priv);
+	/* internal only, will be modified */
+	unsigned int _threads;
 };
 struct stresser_priv {
 	void *fnpriv;		/* function private data */
 	int fnrc;		/* function retval */
-	bool wait;		/* conditional wait? */
+	int wait;		/* conditional wait? */
 	pthread_cond_t *cond;	/* conditional wait */
 	pthread_mutex_t *mutex;	/* conditional wait mutex */
-	struct stresser_unit *u /* stresser unit */
+	pthread_t thread;	/* thread identifier */
+	struct stresser_unit *u;/* stresser unit */
+	int *count;		/* number of initialized threads */
 };
 
-static int _stresser_unit(void *p)
+static void *_stresser_unit(void *p)
 {
 	struct stresser_priv *priv = p;
 	int rc;
 
-	if (p->wait) {
-		rc = pthread_cond_wait(p->cond, p->mutex);
+	if (priv->wait) {
+		pthread_mutex_lock(priv->mutex);
+		*priv->count = *priv->count + 1;
+		rc = pthread_cond_wait(priv->cond, priv->mutex);
+		pthread_mutex_unlock(priv->mutex);
 		if (rc)
-			return rc;
+			return (void *)1;
 	}
 
-	p->fnrc = p->u->fn(p->fnpriv);
+	priv->fnrc = priv->u->fn(priv->fnpriv);
 
-	return 0;
+	return NULL;
 }
 
-static int stresser(struct stress_config *cfg, struct stresser_unit *set,
+static int stresser(struct stresser_config *cfg, struct stresser_unit *set,
 		    int n_unit, void *fnpriv)
 {
 	struct stresser_priv *priv;
-	pthread_cont_t cond = PTHREAD_COND_INITIALIZER;
+	pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-	int threads, i;
+	int threads = 0, i, u, highest, rc = 0, count = 0, tries;
 
 	/* sanity checks */
-	if (cfg->use_chance && n_unit < 2)
-		return EINVAL;
 	if (n_unit == 0)
 		return EINVAL;
 
 	/* first determine the total number of threads */
-	if (cfg->threads)
+	switch (cfg->distribution) {
+	case STRESSD_RANDOM:
+		srand(time(NULL));
 		threads = cfg->threads;
-	else {
+		break;
+	case STRESSD_FIXED:
 		threads = 0;
-		for (i = 0; i < n_unit; i++)
-			threads += set[i].number;
-		if (threads == 0)
-			return EINVAL;
+		for (i = 0; i < n_unit; i++) {
+			threads += set[i].d.number;
+			set[i]._threads = set[i].d.number;
+		}
+		break;
+	case STRESSD_PROP:
+		for (i = 0; i < n_unit; i++) {
+			set[i]._threads =  (set[i].d.proportion * 100) / (cfg->threads * 100);
+			threads += set[i]._threads;
+		}
+		/* if it's not a multiple of 100, just round it */
+		if (threads < cfg->threads)
+			set[0]._threads += (cfg->threads - threads);
+		break;
 	}
+	if (threads == 0)
+		return EINVAL;
 
 	/* allocating private structures */
 	priv = calloc(sizeof(*priv), threads);
 	if (priv == NULL)
 		return ENOMEM;
 
-
 	for (i = 0; i < threads; i++) {
 		priv[i].fnpriv = fnpriv;
+		priv[i].count = &count;
 
 		/* prepare the conditional wait if needed */
-		if (cfg->stress_type == STRESS_HORDE) {
+		if (cfg->stress_type == STRESST_HORDE) {
 			priv[i].cond = &cond;
 			priv[i].mutex = &mutex;
-			priv[i].wait = true;
+			priv[i].wait = 1;
 		}
 
+		switch (cfg->distribution) {
+		case STRESSD_RANDOM:
 		/* if units have chance to happen */
-		if (cfg->use_chance == true) {
-			int u, highest = 0;
-			for (u = 0; u < n_unit; u++) {
-				if (!(rand() % set[u].chance)) {
-					highest = u;
-					break;
+			highest = -1;
+			for (tries = 50; tries; tries--) {
+				int r;
+				r = rand();
+				for (u = 0; (u < n_unit); u++) {
+					if (!(r % (11 - set[u].d.chance))) {
+						if (highest == -1)
+							highest = u;
+						else if (set[u].d.chance > set[highest].d.chance)
+							highest = u;
+					}
 				}
-				if (set[u].chance > set[highest].chance)
-					highest = u;
+				if (highest != -1)
+					break;
 			}
-			priv[i].unit = &set[highest];
-		/* fixed number of unit runs */
-		} else if (!cfg->threads) {
-			int u;
+			if (!tries)
+				highest = 0;
+			priv[i].u = &set[highest];
+			break;
+		/* fixed number of unit runs. proportional has it
+		 * pre-calculated */
+		case STRESSD_FIXED:
+		case STRESSD_PROP:
 			for (u = 0; u < n_unit; u++)
-				if (set[u].number)
+				if (set[u]._threads)
 					break;
 			if (u == n_unit) {
 				fprintf(stderr, "Internal error: %s:%i\n",
 					__FILE__, __LINE__);
 				exit(1);
 			}
-			priv[i].unit = &set[u];
-			set[u].number--;
-		/* proportional unit runs */
-		} else {
-
+			priv[i].u = &set[u];
+			set[u]._threads--;
+			break;
 		}
 	}
+
+	/* all initialized, start the threads */
+	for (i = 0; i < threads; i++) {
+		if (pthread_create(&priv[i].thread, NULL, _stresser_unit,
+		    &priv[i])) {
+			int e = errno;
+			while (--i >= 0)
+				pthread_cancel(priv[i].thread);
+			return e;
+		}
+	}
+
+	/* wait for all threads to start then release them all */
+	if (cfg->stress_type == STRESST_HORDE) {
+		while (1) {
+			pthread_mutex_lock(&mutex);
+			if (count == threads) {
+				pthread_cond_broadcast(&cond);
+				pthread_mutex_unlock(&mutex);
+				break;
+			}
+			pthread_mutex_unlock(&mutex);
+			pthread_yield();
+		}
+	}
+
+	for (i = 0; i < threads; i++) {
+		if (!pthread_join(priv[i].thread, NULL))
+			if (priv[i].fnrc) {
+				fprintf(stderr, "Thread %i returned error "
+					"(%i)\n", i, priv[i].fnrc);
+				rc = 1;
+			}
+	}
+	return rc;
 }
 /* */
 
 static int _setns_stress_thread(void *data)
 {
-
+	printf("one\n");
+	return 0;
 }
 
-static int run_setns_stress(struct config *config)
+static int _setns_stress_thread2(void *data)
 {
-	stressfn set[] = {{__setns_stress_thread, 10}, {NULL,}};
+	printf("two\n");
+	return 0;
+}
+static int _setns_stress_thread3(void *data)
+{
+	printf("three\n");
+	return 0;
+}
 
-	return stresser(config, __setns_stress_thread, NULL);
+static int run_setns_stress(struct stresser_config *config)
+{
+	struct stresser_unit set[] = {
+				      { .d.chance = 2,
+					.fn = _setns_stress_thread, },
+				      { .d.chance = 5,
+					.fn = _setns_stress_thread2, },
+				      { .d.chance = 3,
+					.fn = _setns_stress_thread3, },
+				     };
+
+	return stresser(config, set, 3, NULL);
 }
 
 const char *options = "sh";
 int main(int argc, char *argv[])
 {
-	struct stress_config cfg;
+	struct stresser_config cfg;
 
-	cfg.threads = 1000;
-	cfg.stress_type = STRESS_HORDE;
+	cfg.threads = 10;
+	cfg.distribution = STRESSD_RANDOM;
+	cfg.stress_type = STRESST_HORDE;
 
 	run_setns_stress(&cfg);
 	return 0;
